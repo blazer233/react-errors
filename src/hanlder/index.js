e723e4e3450c7d8516148038b4ce03e210880f75
darylsong <darylsong@tencent.com>
2021-04-14 19:54:20 +0800

    add

--- src/hanlder/index.js
+++ src/hanlder/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import DefaultErrorBoundary from "./core";
-const catchreacterror = (Boundary = DefaultErrorBoundary) => InnerComponent => {
-  return props => (
-    <Boundary {...props}>
-      <InnerComponent {...props} />
-    </Boundary>
-  );
-};
+const catchreacterror = (
+  Boundary = DefaultErrorBoundary
+) => InnerComponent => props => (
+  <Boundary {...props}>
+    <InnerComponent {...props} />
+  </Boundary>
+);
 
 export default catchreacterror;
